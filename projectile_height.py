1581506cdcca809163a5c2fa7590f3b9539f9b5b
matvid <vidonimattia@gmail.com>
2016-08-17 11:21:42 +0200

    Update and rename height_projectile.py to projectile_height.py

---
+++ projectile_height.py
@@ -0,0 +1,10 @@
+import math
+import time
+print("*/*/*/*/*/*/*/*/me*/*/*/*/*/*/*/*")
+time.sleep(5)
+a=float(input("weight in grams of the projectile : "))
+b=float(input("speed in m/s of the projectile: "))
+c=float(input("caliber of the bullet: "))
+d=int(math.sqrt(((a*b)/c)*202.5))
+print("the bullet will come about ",d,"meters")
+time.sleep(5)
